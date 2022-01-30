6c730fbcdbb790d2fa9430dc77924203800acd5d
Clasher-Gaming <40470242+Clasher31HD@users.noreply.github.com>
2022-01-30 21:18:56 +0100

    Add files via upload

---
+++ BoardConfig.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from sm8250-common
+include device/xiaomi/sm8250-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/xiaomi/lmi
+
+BUILD_BROKEN_DUP_RULES := true
+
+# Display
+TARGET_SCREEN_DENSITY := 440
+
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_lmi
+TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_lmi
+
+# Kernel
+TARGET_KERNEL_CONFIG := vendor/lmi_defconfig
+
+# OTA assert
+TARGET_OTA_ASSERT_DEVICE := lmi,lmipro
+
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
+# Inherit from the proprietary version
+include vendor/xiaomi/lmi/BoardConfigVendor.mk
---
+++ device.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Fingerprint
+TARGET_HAS_FOD := true
+
+# Inherit from sm8250-common
+$(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-cherish
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+# Audio configs
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.barometer.xml
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Inherit from vendor blobs
+$(call inherit-product, vendor/xiaomi/lmi/lmi-vendor.mk)
